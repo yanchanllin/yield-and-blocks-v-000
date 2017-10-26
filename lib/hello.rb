9e0422927a033338461cf075d841f089c70361ee
YanChan  Lin <yonalin1@gmail.com>
2017-10-26 17:49:04 +0000

    automatically backed up by learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,12 +1,12 @@
-# def hello_t(array)
-#   i = 0
-#    
-#     while i < array.length
-#        yield array[i]
-#       i = i + 1
-#     end
-# end
-# 
+def hello_t(array)
+  i = 0
+
+    while i < array.length
+       yield array[i]
+      i = i + 1
+    end
+end
+#
 # ["Tim", "Tom", "Jim"]
 hello_t(["Tim", "Tom", "Jim"]) do |name|
   if name.start_with?("T")
@@ -14,4 +14,3 @@ hello_t(["Tim", "Tom", "Jim"]) do |name|
   end
 end
 # call your method here!
-
