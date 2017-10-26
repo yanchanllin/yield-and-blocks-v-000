f2f19704de3a709defb9d404a8927d68cf3a28b4
YanChan  Lin <yonalin1@gmail.com>
2017-10-26 17:54:38 +0000

    automatically backed up by learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -6,12 +6,13 @@ def hello_t(array)
        yield array[i]
       i = i + 1
     end
-    
+
     array
   else
   puts "Hey! No block was given!"
-end
-#
+ end
+end 
+
 # ["Tim", "Tom", "Jim"]
 # hello_t(["Tim", "Tom", "Jim"]) do |name|
 #   if name.start_with?("T")
