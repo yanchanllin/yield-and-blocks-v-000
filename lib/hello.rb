b8fabd96839aa215b31e24c887d92ea23815aa29
YanChan  Lin <yonalin1@gmail.com>
2017-10-26 17:53:32 +0000

    automatically backed up by learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -13,9 +13,8 @@ def hello_t(array)
 end
 #
 # ["Tim", "Tom", "Jim"]
-hello_t(["Tim", "Tom", "Jim"]) do |name|
-  if name.start_with?("T")
-    puts "Hi, #{name}"
-  end
-end
-# call your method here!
+# hello_t(["Tim", "Tom", "Jim"]) do |name|
+#   if name.start_with?("T")
+#     puts "Hi, #{name}"
+#   end
+# end
